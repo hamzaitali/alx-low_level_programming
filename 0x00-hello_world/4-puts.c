baec251f3b4c3473feabaaea4da4d3b55e5a8f62
hamzaitali <hmz.aitali@gmail.com>
2023-06-15 18:07:24 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-	
- int main(void)
+/**
+ * main - program that prints a sentence with puts function
+ * description: needit to validate betty
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
