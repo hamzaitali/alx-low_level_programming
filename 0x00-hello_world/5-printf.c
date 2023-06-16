e73d7c75a249c3224bfecd0cd301583172ea2d47
hamzaitali <hmz.aitali@gmail.com>
2023-06-15 18:24:40 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - his program printf a sentence
+ *
+ * Description: can be print using printf
+ *
+ * Return: 0 (suceed)
+ **/
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
