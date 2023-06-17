83b4c523a75dfa3019936fe1be8be3a01ca61672
hamzaitali <hmz.aitali@gmail.com>
2023-06-16 19:39:56 -0700

    dsfsd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: desc
+ * Return: Always 0
+ **/
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 79)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar ('\n');
+	return (0);
+}
