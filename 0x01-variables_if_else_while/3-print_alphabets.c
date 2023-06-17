09c250317833f7245d57529bc4ebbe74d9a733c8
hamzaitali <hmz.aitali@gmail.com>
2023-06-16 18:30:15 -0700

    alpPHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: show alphabet in lowercase followed by uppercase and a $
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	char a1 = 'a';
+	char a2 = 'A';
+
+	while (a1 <= 'z')
+	{
+		putchar(a1);
+		a1++;
+	}
+	while (a2 <= 'Z')
+	{
+		putchar(a2);
+		a2++;
+	}
+	putchar('$');
+	return (0);
+}
