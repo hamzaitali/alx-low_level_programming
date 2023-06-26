d911fcbbfa21a58c6c77393f3db5758457982d37
hamzaitali <hmz.aitali@gmail.com>
2023-06-26 16:49:13 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - receive a number and set it to 99
+ *
+ * Description: set a number to 98
+ * @n: the number edited
+ * Return: always 0
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int reset_to_98(int *n);
+
+#endif
