b1e74318e15792b2d8b7ba932ac78cebb7af4aac
hamzaitali <hmz.aitali@gmail.com>
2023-06-26 17:35:32 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strlen - return the length of a string
+ * Description: return lenght of a string
+ * @c : the charactere received
+ *
+ * Return: Always int
+ **/
+
+int _strlen(char *c)
+{
+	int i = strlen(c);
+
+	return (i);
+}
