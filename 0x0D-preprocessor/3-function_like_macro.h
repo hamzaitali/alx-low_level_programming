8af16b656709f0543072d60a5e36e86cc4b3889b
hamzaitali <hmz.aitali@gmail.com>
2023-07-17 16:47:34 -0700

    all in one

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
-#include <stdio.h>
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
